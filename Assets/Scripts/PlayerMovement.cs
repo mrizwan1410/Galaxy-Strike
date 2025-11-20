93c751ab56aae45fc0bbf134b2033bc86621f83a
RIZWAN <morizwan1410@gmail.com>
2025-11-20 18:44:54 +0530

    Input System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float controlSpeed;
+    Vector2 movement;
+   
     void Update()
     {
-        
+        ProcessTranslation();
     }
 
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x - xOffset, transform.localPosition.y + yOffset, 0f);
     }
 }
