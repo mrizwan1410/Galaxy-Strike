bea367606253538a20288d87be49e08628fce33e
RIZWAN <morizwan1410@gmail.com>
2025-11-20 19:03:19 +0530

    Clamping movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed;
+    [SerializeField] float xRange = 20f;
+    [SerializeField] float yRange = 8f;
     Vector2 movement;
    
     void Update()
@@ -19,7 +21,13 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x - xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
         float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x - xOffset, transform.localPosition.y + yOffset, 0f);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 }
