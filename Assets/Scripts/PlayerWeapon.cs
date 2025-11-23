c74d3e0c5e43548162b5fd52b1ca04915a6dfe68
RIZWAN <morizwan1410@gmail.com>
2025-11-23 18:18:14 +0530

    Firing Mechanism

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] GameObject laser;
+    bool isFiring = false;
+
+    private void Update()
+    {
+        ProcessFiring();
+    }
+    public void OnFire(InputValue value)
+    {
+        isFiring = value.isPressed;
+    }
+
+    void ProcessFiring()
+    {
+        var emissionModule = laser.GetComponent<ParticleSystem>().emission;
+        emissionModule.enabled = isFiring;
+    }
+}
