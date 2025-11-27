e708d4e764be1d0bafeefae2084952308830efc3
RIZWAN <morizwan1410@gmail.com>
2025-11-27 19:47:01 +0530

    player vfx

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] GameObject destroyedVFX;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"Hit {other.gameObject.name}");
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
